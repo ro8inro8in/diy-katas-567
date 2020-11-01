5f40f08fb2b2ae43de64eeb25d0ae58136ebf3c8
Robin Edwards <robinedwards1987@gmail.com>
2020-11-01 18:29:54 +0000

    Final Commit

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,35 @@
-const { getEmployerRole } = require("../src");
+const { getEmployerRole } = require("../src");  
+  const employees = [{
+    name: 'Satti',
+    role: 'Developer'
+}, {
+    name: 'Jenny',
+    role: 'Sales Associate'
+}, {
+    name: 'Javid',
+    role: 'Human Recommended Reading Assistant'
+}]
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole('Javid', employees)).toBe('Human Recommended Reading Assistant');
+    expect(getEmployerRole('Satti', employees)).toBe('Developer');
+    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate');
+    
+  });
+      
 });
+
+
+
+// npm t -- getEmployerRole
+
+
+
+//Function with 2 parameters including a employeeName(string) and an employees(array)
+
+///Write a function that takes in an array of objects and a string- which is a name
+
+////And we need to return from the function the employee role 
+
+// e.g  getEmployerRole('Satti', employees) should return 'Developer'
